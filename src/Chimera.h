eff49647cbabad634ca4bfbb0110a72697ca92fd
Sergey Radionov <RSATom@gmail.com>
2014-08-25 22:16:45 +0700

    fullscreen mode is optional...

--- src/Chimera.h
+++ src/Chimera.h
@@ -74,9 +74,9 @@ public:
     int add_playlist_item( const std::string& mrl, const std::vector<std::string>& options );
 
 public:
-    virtual bool is_fullscreen() = 0;
-    virtual void set_fullscreen( bool fs ) = 0;
-    virtual void toggle_fullscreen() = 0;
+    virtual bool is_fullscreen() { return false; };
+    virtual void set_fullscreen( bool fs ) {};
+    virtual void toggle_fullscreen() {};
     std::string getQmlError();
 
 private:
