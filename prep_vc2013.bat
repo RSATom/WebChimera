2ff4debc0df6beaef6dff3d428c6ea622a9ea03b
Sergey Radionov <RSATom@gmail.com>
2014-09-20 20:54:59 +0700

    don't show cmake warnings for developers

--- prep_vc2013.bat
+++ prep_vc2013.bat
@@ -2,6 +2,6 @@
 for %%i in ("%~0") do set THIS_BAT_PATH=%%~dpi
 set SAVE_CD=%CD%
 cd %THIS_BAT_PATH%\deps\firebreath\
-call prep2013.cmd "..\..\src" "..\..\build_win_vc2013"
+call prep2013.cmd "..\..\src" "..\..\build_win_vc2013" -Wno-dev
 cd %SAVE_CD%
 pause
