8184c99cdd0dbfa3dfc0a4c0a7d336e31c59562e
Sergey Radionov <RSATom@gmail.com>
2014-11-22 01:14:06 +0600

    prepare for FBVLC emulation

--- src/PluginConfig.cmake
+++ src/PluginConfig.cmake
@@ -11,12 +11,14 @@ set(COPYRIGHT_YEARS "2014")
 
 set(PLUGIN_NAME "WebChimera")
 set(PLUGIN_PREFIX "WebChimera")
-set(PLUGIN_DESCRIPTION "Web Plugin powered by Firebreath/Qt Qml/Vlc")
+set( PLUGIN_DESCRIPTION "Web Plugin powered by Firebreath/Qt Qml/Vlc" )
+set( FBVLC_PLUGIN_DESCRIPTION "FireBreathed VLC (aka FBVLC) emulator" )
 set(PRODUCT_NAME "Chimera Web Plugin")
 set(PROJECT_OWNER "Sergey Radionov")
 set(PROJECT_DOMAIN "WebChimera.org")
 
-set(PLUGIN_MIMETYPE "application/x-chimera-plugin")
+set( PLUGIN_MIMETYPE "application/x-chimera-plugin" )
+set( FBVLC_PLUGIN_MIMETYPE "application/x-fb-vlc" )
 
 # ActiveX constants:
 set(FBTYPELIB_NAME ${PLUGIN_NAME}Lib)
@@ -31,7 +33,10 @@ set(AXVERSION_NUM "${PLUGIN_VERSION}")
 # NOTE: THESE GUIDS *MUST* BE UNIQUE TO YOUR PLUGIN/ACTIVEX CONTROL!  YES, ALL OF THEM!
 set(FBTYPELIB_GUID 2b642482-539b-5158-b60c-725219b3a4b6)
 set(IFBControl_GUID 0d25ef13-5e83-527e-822e-e97f89d9055f)
-set(FBControl_GUID df4501d0-6a9f-5d59-a08d-7be21a13c847)
+set( FBControl_GUID
+     df4501d0-6a9f-5d59-a08d-7be21a13c847
+     dc5b1d57-cf58-4500-b57e-fe62bc00fd64 ) #for FBVLC emulator
+
 set(IFBComJavascriptObject_GUID 3321ffa1-0fcd-5c02-bf77-a966735a2296)
 set(FBComJavascriptObject_GUID d873591c-3064-53b2-afe9-e67d54974423)
 set(IFBComEventSource_GUID 90a5b047-5212-5080-b565-1a0d7bdc5946)
@@ -42,7 +47,9 @@ else( FB_PLATFORM_ARCH_32 )
 endif( FB_PLATFORM_ARCH_32 )
 
 # these are the pieces that are relevant to using it from Javascript
-set(ACTIVEX_PROGID "${PLUGIN_NAME}.${PLUGIN_NAME}")
+set( ACTIVEX_PROGID
+     "${PLUGIN_NAME}.${PLUGIN_NAME}"
+     "${PLUGIN_NAME}.FBVLC" )
 if( FB_PLATFORM_ARCH_32 )
     set(MOZILLA_PLUGINID "${PROJECT_DOMAIN}/${PLUGIN_NAME}")  # No 32bit postfix to maintain backward compatability.
 else( FB_PLATFORM_ARCH_32 )
@@ -51,7 +58,10 @@ endif( FB_PLATFORM_ARCH_32 )
 
 # strings
 set(FBSTRING_CompanyName "${PROJECT_OWNER}")
-set(FBSTRING_PluginDescription "${PLUGIN_DESCRIPTION}")
+set( FBSTRING_PluginDescription
+     "${PLUGIN_DESCRIPTION}"
+     "${FBVLC_PLUGIN_DESCRIPTION}"
+     )
 set(FBSTRING_PLUGIN_VERSION "${PLUGIN_VERSION}")
 set(FBSTRING_LegalCopyright "Copyright ${COPYRIGHT_YEARS} ${PROJECT_OWNER}")
 if( WIN32 )
@@ -68,7 +78,9 @@ if( FB_PLATFORM_ARCH_32 )
 else( FB_PLATFORM_ARCH_32 )
     set(FBSTRING_PluginName "${PLUGIN_NAME} ${FB_PLATFORM_ARCH_NAME}")
 endif( FB_PLATFORM_ARCH_32 )
-set(FBSTRING_MIMEType "${PLUGIN_MIMETYPE}")
+set( FBSTRING_MIMEType
+     "${PLUGIN_MIMETYPE}"
+     "${FBVLC_PLUGIN_MIMETYPE}" )
 
 # Uncomment this next line if you're not planning on your plugin doing
 # any drawing:
