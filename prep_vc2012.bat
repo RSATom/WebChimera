5c2fe2694eec4133c59c745f580b9f5b44165f95
Sergey Radionov <RSATom@gmail.com>
2014-11-22 23:37:58 +0600

    don't need to support vc2012 anymore

--- prep_vc2012.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-for %%i in ("%~0") do set THIS_BAT_PATH=%%~dpi
-set SAVE_CD=%CD%
-cd %THIS_BAT_PATH%\deps\firebreath\
-call prep2012.cmd "..\..\src" "..\..\build_win_vc2012" -Wno-dev
-cd %SAVE_CD%
-pause
