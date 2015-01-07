9c20d74c1e7ec24807de9e925177aca19ceff945
Sergey Radionov <RSATom@gmail.com>
2015-01-07 12:40:54 +0600

    macqtconfigure.sh added

---
+++ macqtconfigure.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+./configure -prefix $HOME/Qt-5.4.0-static -opensource -confirm-license -static -release -no-framework -no-sql-sqlite -no-qml-debug -nomake examples -nomake tests -skip qtactiveqt -skip qtenginio -skip qtlocation -skip qtmultimedia -skip qtserialport -skip qtquick1 -skip qtscript -skip qtsensors -skip qtwebengine -skip qtwebkit -skip qtwebkit-examples -skip qtwebsockets -skip qtxmlpatterns -skip qtlocation -no-widgets
+
+
