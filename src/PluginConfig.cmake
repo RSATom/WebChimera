8b058b94c2fc70e75a73595df6db786ca2126f1c
Sergey Radionov <RSATom@gmail.com>
2015-01-08 15:22:21 +0600

    version 0.2.2

--- src/PluginConfig.cmake
+++ src/PluginConfig.cmake
@@ -5,7 +5,7 @@
 #
 #\**********************************************************/
 
-set(PLUGIN_VERSION "0.2.0")
+set(PLUGIN_VERSION "0.2.2")
 set(VLC_VERSION "2.1.5")
 set(COPYRIGHT_YEARS "2011-2014")
 
