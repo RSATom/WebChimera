520322193abc8b69de1f4567c690eef579416391
Sergey Radionov <RSATom@gmail.com>
2015-04-16 22:50:33 +0600

    Mac: use Qt 5.4.1

--- prep_mac_qt54.sh
+++ prep_mac_qt54.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-deps/firebreath/prepmac.sh src build_mac -D CMAKE_OSX_ARCHITECTURES="x86_64" -DCMAKE_PREFIX_PATH=~/Qt-5.4.0-static -DQT_STATIC=1 -Wno-dev
+deps/firebreath/prepmac.sh src build_mac -D CMAKE_OSX_ARCHITECTURES="x86_64" -DCMAKE_PREFIX_PATH=~/Qt-5.4.1-static -DQT_STATIC=1 -Wno-dev
